ba16161a8ccb4af38687a172504b51713a7ee139
kirill_barkhatov <kirill_barkhatov@hotmail.com>
2024-05-29 21:37:44 +0300

    добавлен код для тестирования

--- src/widget.py
+++ src/widget.py
@@ -10,14 +10,15 @@ def masked_numbers(card_or_account: str) -> str:
 
 
 #тестирование
-test_data = ["Maestro 1596837868705199",
-                "Счет 64686473678894779589",
-                "MasterCard 7158300734726758",
-                "Счет 35383033474447895560",
-                "Visa Classic 6831982476737658",
-                "Visa Platinum 8990922113665229",
-                "Visa Gold 5999414228426353",
-                "Счет 73654108430135874305"]
+if __name__ in '__main__':
+    test_data = ["Maestro 1596837868705199",
+                    "Счет 64686473678894779589",
+                    "MasterCard 7158300734726758",
+                    "Счет 35383033474447895560",
+                    "Visa Classic 6831982476737658",
+                    "Visa Platinum 8990922113665229",
+                    "Visa Gold 5999414228426353",
+                    "Счет 73654108430135874305"]
 
-for data in test_data:
-    print(masked_numbers(data))
+    for data in test_data:
+        print(masked_numbers(data))
