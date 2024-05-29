2810868b04339b58a58131f854c31eba532d093a
kirill_barkhatov <kirill_barkhatov@hotmail.com>
2024-05-29 20:37:42 +0300

    добавлена функция masked_numbers

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from masks import masked_card_number, masked_account_number
+
+def masked_numbers(card_or_account: str) -> str:
+    """Функция принимает данные карты или счета и маскирует часть их номера"""
+
+    if card_or_account[:4] == "Счет":
+        return f"{card_or_account[:5]}{masked_account_number(card_or_account[5:])}"
+    return f"{card_or_account[:-16]}{masked_card_number(card_or_account[-16:])}"
+
+
+
+#тестирование
+test_data = ["Maestro 1596837868705199",
+                "Счет 64686473678894779589",
+                "MasterCard 7158300734726758",
+                "Счет 35383033474447895560",
+                "Visa Classic 6831982476737658",
+                "Visa Platinum 8990922113665229",
+                "Visa Gold 5999414228426353",
+                "Счет 73654108430135874305"]
+
+for data in test_data:
+    print(masked_numbers(data))
