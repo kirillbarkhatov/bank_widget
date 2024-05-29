b805bb8a6a60b509599279b291b36c3e7da79a6f
kirill_barkhatov <kirill_barkhatov@hotmail.com>
2024-05-29 20:13:11 +0300

    git init

---
+++ src/masks.py
@@ -0,0 +1,14 @@
+def masked_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску формате XXXX XX** **** XXXX"""
+
+    return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+
+
+def masked_account_number(account_number: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску в формате **XXXX"""
+
+    return f"**{str(account_number)[-4:]}"
+
+
+print(masked_card_number(7000792289606361))
+print(masked_account_number(73654108430135874305))
