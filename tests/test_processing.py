8d57c2cc64cc17e2c9159099469d5d1e7e851101
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-06-07 20:36:18 +0300

    Сделаны тесты для processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,55 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.fixture
+def operations():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+@pytest.mark.parametrize("state, filtered_data",
+                         [
+                            ('EXECUTED',
+                             [
+                                 {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+                             ]
+                            ),
+                            ('CANCELED',
+                             [
+                                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+                             ]
+                            )
+                        ])
+def test_filter_by_state(operations, state, filtered_data):
+    assert filter_by_state(operations, state) == filtered_data
+
+
+@pytest.mark.parametrize("is_reverse, sorted_data",
+                         [
+                             (False,
+                              [
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+                              ]
+                              ),
+
+                             (True,
+                              [
+                                  {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+                              ]
+                              )
+                         ])
+
+def test_sort_by_date(operations, is_reverse, sorted_data):
+    assert sort_by_date(operations, is_reverse) == sorted_data
