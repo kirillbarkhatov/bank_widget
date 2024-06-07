edd26ee24ca171f783a27776b72f3318bcb4c10e
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-06-07 20:40:18 +0300

    Форматирование и проверка линтерами

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,14 +7,15 @@ from src.masks import masked_account_number, masked_card_number
 def account_number():
     return 73654108430135874305
 
+
 @pytest.fixture
 def card_number():
     return 7000792289606361
 
 
 def test_masked_account_number(account_number):
-    assert masked_account_number(account_number) == '**4305'
+    assert masked_account_number(account_number) == "**4305"
 
 
 def test_masked_card_number(card_number):
-    assert masked_card_number(card_number) == '7000 79** **** 6361'
+    assert masked_card_number(card_number) == "7000 79** **** 6361"
