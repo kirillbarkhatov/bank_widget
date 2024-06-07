43d788e3729ab377d95bc80595c1e370498f2585
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-06-07 19:20:59 +0300

    Сделаны тесты для widget.py

--- src/widget.py
+++ src/widget.py
@@ -19,20 +19,20 @@ def convert_date(date_iso: str) -> str:
 
 
 # тестирование
-if __name__ in "__main__":
-    test_data = [
-        "Maestro 1596837868705199",
-        "Счет 64686473678894779589",
-        "MasterCard 7158300734726758",
-        "Счет 35383033474447895560",
-        "Visa Classic 6831982476737658",
-        "Visa Platinum 8990922113665229",
-        "Visa Gold 5999414228426353",
-        "Счет 73654108430135874305",
-    ]
-
-    for data in test_data:
-        print(masked_numbers(data))
-
-    test_date = "2018-07-11T02:26:18.671407"
-    print(convert_date(test_date))
+# if __name__ in "__main__":
+#     test_data = [
+#         "Maestro 1596837868705199",
+#         "Счет 64686473678894779589",
+#         "MasterCard 7158300734726758",
+#         "Счет 35383033474447895560",
+#         "Visa Classic 6831982476737658",
+#         "Visa Platinum 8990922113665229",
+#         "Visa Gold 5999414228426353",
+#         "Счет 73654108430135874305",
+#     ]
+#
+#     for data in test_data:
+#         print(masked_numbers(data))
+#
+#     test_date = "2018-07-11T02:26:18.671407"
+#     print(convert_date(test_date))
