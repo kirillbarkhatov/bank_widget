9af1b1ba1ee51f29fd1e32dfe6a47bfc86e5dc08
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-06-12 00:51:47 +0300

    Добавлена функция card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -121,3 +121,17 @@ for _ in range(5):
 # Перевод со счета на счет
 # Перевод с карты на карту
 # Перевод организации
+
+
+def card_number_generator(start: int, end: int) -> str:
+    """Генератор номеров банковских карты - диапазоны передаются как параметры генератора
+    """
+    card_number = start
+    while card_number <= end:
+        length = len(str(card_number))
+        full_card_number = "0" * (16 - length) + str(card_number)
+        yield f'{full_card_number[:4]} {full_card_number[4:8]} {full_card_number[8:12]} {full_card_number[12:]}'
+        card_number += 1
+
+for card_number in card_number_generator(13, 20):
+    print(card_number)
