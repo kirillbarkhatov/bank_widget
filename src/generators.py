dd7f7867df7c6a5dcb7ed4faa88187c33544b41d
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-06-12 01:47:21 +0300

    Отформатировано

--- src/generators.py
+++ src/generators.py
@@ -1,107 +1,19 @@
-transactions = (
-    [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
-    ]
-)
+from typing import Generator
 
-def filter_by_currency(transactions: list[dict], currency: str) -> int:
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Generator[dict, None, None]:
     """Функция - генератор принимает список словарей с банковскими операциями
     (или объект-генератор, который выдает по одной банковской операции)
     и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта
     """
-    filtered_transactions = (transaction
-          for transaction in transactions
-          if transaction["operationAmount"]["currency"]["code"] == currency
-          )
+    filtered_transactions = (
+        transaction for transaction in transactions if transaction["operationAmount"]["currency"]["code"] == currency
+    )
     for filtered_transaction in filtered_transactions:
         yield filtered_transaction
 
-usd_transactions = filter_by_currency(transactions, "USD")
 
-
-
-for _ in range(2):
-    print(next(usd_transactions)["id"])
-
-# Пример вывода
-# 939719570
-# 142264268
-
-def transaction_descriptions(transactions: list[dict]) -> str:
+def transaction_descriptions(transactions: list[dict]) -> Generator[str, None, None]:
     """Функция - генератор, который принимает список словарей
     и возвращает описание каждой операции по очереди
     """
@@ -110,28 +22,11 @@ def transaction_descriptions(transactions: list[dict]) -> str:
         yield transaction["description"]
 
 
-descriptions = transaction_descriptions(transactions)
-
-for _ in range(5):
-    print(next(descriptions))
-
-
-# Перевод организации
-# Перевод со счета на счет
-# Перевод со счета на счет
-# Перевод с карты на карту
-# Перевод организации
-
-
-def card_number_generator(start: int, end: int) -> str:
-    """Генератор номеров банковских карты - диапазоны передаются как параметры генератора
-    """
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
+    """Генератор номеров банковских карты - диапазоны передаются как параметры генератора"""
     card_number = start
     while card_number <= end:
         length = len(str(card_number))
         full_card_number = "0" * (16 - length) + str(card_number)
-        yield f'{full_card_number[:4]} {full_card_number[4:8]} {full_card_number[8:12]} {full_card_number[12:]}'
+        yield f"{full_card_number[:4]} {full_card_number[4:8]} {full_card_number[8:12]} {full_card_number[12:]}"
         card_number += 1
-
-for card_number in card_number_generator(1, 5):
-    print(card_number)
--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,11 @@
-def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает на вход список словарей и значение для ключа
     state (опциональный параметр со значением по умолчанию EXECUTED)
     и возвращает новый список, содержащий только те словари,
     у которых ключ state содержит переданное в функцию значение
-     """
+    """
 
-    return [
-        operation
-        for operation in operations
-        if operation['state'] == state
-    ]
+    return [operation for operation in operations if operation["state"] == state]
 
 
 def sort_by_date(operations: list[dict], is_reverse: bool = True) -> list[dict]:
@@ -19,10 +15,7 @@ def sort_by_date(operations: list[dict], is_reverse: bool = True) -> list[dict]:
     порядок сортировки (убывание, возрастание)
     """
 
-    return [
-        operation
-        for operation in sorted(operations, key=lambda x: x['date'], reverse=is_reverse)
-    ]
+    return [operation for operation in sorted(operations, key=lambda x: x["date"], reverse=is_reverse)]
 
 
 # тестирование
