d7de980119c78a8053d592891e382c8193cd4c1c
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-06-12 00:26:42 +0300

    Добавлена функция filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -78,5 +78,27 @@ transactions = (
     ]
 )
 
+def filter_by_currency(transactions: list[dict], currency: str) -> int:
+    """Функция - генератор принимает список словарей с банковскими операциями
+    (или объект-генератор, который выдает по одной банковской операции)
+    и возвращает итератор, который выдает по очереди операции, в которых указана заданная валюта
+    """
+    filtered_transactions = (transaction
+          for transaction in transactions
+          if transaction["operationAmount"]["currency"]["code"] == currency
+          )
+    for filtered_transaction in filtered_transactions:
+        yield filtered_transaction
+
+usd_transactions = filter_by_currency(transactions, "USD")
+
+
+
+for _ in range(2):
+    print(next(usd_transactions)["id"])
+
+# Пример вывода
+# 939719570
+# 142264268
 
 
