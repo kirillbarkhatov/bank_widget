df19c1c3b18c4de1affd968e251292544ba8a373
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-06-26 00:45:48 +0300

    Добавлен функционал вывода размера транзакции в рублях. Обновлен README

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import json
 
+from src.external_api import currency_converter
+
 
 def get_transactions_from_json(file_path: str) -> list[dict]:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
@@ -17,3 +19,18 @@ def get_transactions_from_json(file_path: str) -> list[dict]:
 
     except FileNotFoundError:
         return []
+
+
+def get_transaction_amount(transaction: dict) -> float:
+    """Функция принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
+    и конвертации суммы операции в рубли
+    """
+
+    transaction_amount = float(transaction["operationAmount"]["amount"])
+    transaction_currency = transaction["operationAmount"]["currency"]["code"]
+
+    if transaction_currency == "RUB":
+        return transaction_amount
+
+    return currency_converter(transaction_amount, transaction_currency)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 import pytest
 
-from src.utils import get_transactions_from_json
+from src.utils import get_transaction_amount, get_transactions_from_json
 
 
 @pytest.fixture
@@ -38,3 +38,34 @@ def test_get_transactions_from_json_file_exist(path_file_success):
 # Тест при условии отсутствия файла
 def test_get_transactions_from_json_file_failure(path_file_failure):
     assert get_transactions_from_json(path_file_failure) == []
+
+
+@pytest.mark.parametrize(
+    "transaction, amount",
+    [
+        ({"operationAmount": {"amount": "31957.58", "currency": {"code": "RUB"}}}, 31957.58),
+        ({"operationAmount": {"amount": "433.11", "currency": {"code": "RUB"}}}, 433.11),
+    ],
+)
+def test_get_transaction_amount_rub(transaction, amount):
+    assert get_transaction_amount(transaction) == amount
+
+
+@pytest.fixture
+def transaction_usd():
+    return {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560",
+    }
+
+
+def test_get_transaction_amount_currency(transaction_usd):
+    with patch("src.external_api.requests.get") as mock_get:
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {"rates": {"RUB": 100}}
+        assert get_transaction_amount(transaction_usd) == 822137.00
