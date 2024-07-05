2024fed4110e7177b1f98f0e349f24c05159fac0
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-07-05 23:27:49 +0300

    Обновление логики маскирования карт и счетов

--- src/masks.py
+++ src/masks.py
@@ -9,29 +9,27 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-def masked_card_number(card_number: int) -> str:
+def masked_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску формате XXXX XX** **** XXXX"""
 
-    # if type(card_number) is int and 1 <= card_number // (10**15) <= 9:
-    #     mask_card_number = f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
-    #     logger.info(f"Выполнено маскирование номера карты - результат: {mask_card_number}")
-    #     return mask_card_number
-    # logger.error(f"Не удалось выполнить маскирование номера карты - передан неверный формат карты: {card_number}")
-    # raise Exception("Передан неверный формат карты")
+    if type(card_number) is str and len(card_number) == 16 and card_number.isdigit():
+        mask_card_number = f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+        logger.info(f"Выполнено маскирование номера карты {card_number}- результат: {mask_card_number}")
+        return mask_card_number
+    logger.error(f"Не удалось выполнить маскирование номера карты - передан неверный формат карты: {card_number}")
+    raise Exception("Передан неверный формат карты")
 
-    return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
 
-def masked_account_number(account_number: int) -> str:
+def masked_account_number(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску в формате **XXXX"""
 
-    # if type(account_number) is int and 1 <= account_number // (10**19) <= 9:
-    #     mask_account_number = f"**{str(account_number)[-4:]}"
-    #     logger.info(f"Выполнено маскирование номера счета - результат: {mask_account_number}")
-    #     return mask_account_number
-    # logger.error(f"Не удалось выполнить маскирование номера счета - передан неверный формат счета: {account_number}")
-    # raise Exception("Передан неверный формат счета")
+    if type(account_number) is str and len(account_number) == 20 and account_number.isdigit():
+        mask_account_number = f"**{str(account_number)[-4:]}"
+        logger.info(f"Выполнено маскирование номера счета {account_number} - результат: {mask_account_number}")
+        return mask_account_number
+    logger.error(f"Не удалось выполнить маскирование номера счета - передан неверный формат счета: {account_number}")
+    raise Exception("Передан неверный формат счета")
 
-    return f"**{str(account_number)[-4:]}"
 
 # для проверки
 # print(masked_card_number(7000792289606361))
